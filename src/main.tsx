350905db1162c185b28397cd1b1b1f080a591299
w4sk <guiguidubian@gmail.com>
2025-01-02 19:44:28 +0900

    tiny fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
